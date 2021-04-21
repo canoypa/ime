feb0b7b11a968f7999fb23208638603f2f5a5195
Cano <contact.sasasaky@gmail.com>
2021-04-21 16:45:09 +0900

    ビルドスクリプトの改善

--- scripts/build.js
+++ scripts/build.js
@@ -2,63 +2,78 @@ const archiver = require("archiver");
 const path = require("path");
 const fs = require("fs");
 
-const srcPath = path.resolve("src");
-const distPath = "dist";
-
-const createPart = (partsPath) => {
-  const name = path.basename(partsPath);
-
-  const parts = fs
-    .readdirSync(partsPath)
-    .map((part) => path.join(partsPath, part));
-
-  const text = parts.reduce((acc, cur) => {
-    if (fs.statSync(cur).isDirectory()) return acc + createPart(cur).text;
-
-    const raw = fs.readFileSync(cur, { encoding: "utf-8" });
-    return (
-      acc +
-      raw
-        .replace(/^\n|#.+\n/gm, "") // 空行とコメント削除
-        .replace(/,(?:\s+)?/gm, "	") // xx, yy を xx  yy に変更
-    );
-  }, "");
-
-  return { name, text };
+// 単一ファイルを Set<[よみ, 単語, 品詞]> に変換
+const parseFile = (file) => {
+  return new Set(
+    file
+      .replace(/#.+/gm, "") // コメント行削除
+      .replace(/^\r\n/gm, "") // 空行削除
+      .replace(/\r\n$/, "") // 末端改行を削除
+      .split(/\r\n/gm) // 改行で分解
+      .map((v) => v.split(/,\s+/gm)) // よみ, 単語, 品詞 に分解
+  );
 };
 
-const collect = (partsPaths) => {
-  const parts = partsPaths.map((partsPath) => createPart(partsPath));
-  const all = parts.reduce((acc, cur) => acc + cur.text, "");
-  return { all, parts };
+// 子ディレクトリ/ファイルを再帰的に探索し、Set<[よみ, 単語, 品詞]> を返す
+const convertDic = (fileOrFolderPath) => {
+  const sett = new Set();
+
+  const pathState = fs.statSync(fileOrFolderPath);
+  const isDirectory = pathState.isDirectory();
+
+  if (isDirectory) {
+    fs.readdirSync(fileOrFolderPath).forEach((p) => {
+      const set = convertDic(path.join(fileOrFolderPath, p));
+      set.forEach((v) => {
+        sett.add(v);
+      });
+    });
+  } else {
+    const file = fs.readFileSync(fileOrFolderPath, { encoding: "utf-8" });
+    parseFile(file).forEach((v) => {
+      sett.add(v);
+    });
+  }
+
+  return sett;
 };
 
-const createDic = (dicSrcPath) => {
+// Set<[...]> をテキストに変換
+const convertText = (set) => {
+  // 最後に改行が必要
+  return [...set].map((v) => v.join("	")).join("\n") + "\n";
+};
+
+// convertDic してテキスト化して zip する
+const createDic = (distPath, dicPath) => {
   const zip = archiver("zip", { zlib: { level: 9 } });
+  const outFileName = `${path.basename(dicPath)}.zip`;
+  const output = fs.createWriteStream(path.resolve(distPath, outFileName));
 
-  const outputName = `${path.basename(dicSrcPath)}.zip`;
-  const output = fs.createWriteStream(path.resolve(distPath, outputName));
-
-  const docPartsPaths = fs
-    .readdirSync(dicSrcPath)
-    .map((dir) => path.join(dicSrcPath, dir));
-  const c = collect(docPartsPaths);
+  const all = convertText(convertDic(dicPath));
 
   zip.pipe(output);
-  zip.append(c.all, { name: `${path.basename(dicSrcPath)}.txt` });
-  c.parts.forEach((part) => {
-    const name = `parts/${part.name}.txt`;
-    zip.append(part.text, { name: name });
+  zip.append(all, { name: `${path.basename(dicPath)}.txt` });
+
+  fs.readdirSync(dicPath).forEach((partLabel) => {
+    const partPath = path.join(dicPath, partLabel);
+    const part = convertText(convertDic(partPath));
+    zip.append(part, { name: `parts/${partLabel}.txt` });
   });
   zip.finalize();
 };
 
-const init = (srcPath) => {
-  const distDir = path.resolve(distPath);
-  if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
+const main = () => {
+  const srcPath = path.resolve("src");
+  const distPath = path.resolve("dist");
+
+  if (!fs.existsSync(distPath)) fs.mkdirSync(distPath);
 
   const srcDir = fs.readdirSync(srcPath);
-  srcDir.forEach((dicSrcPath) => createDic(path.join(srcPath, dicSrcPath)));
+  srcDir.forEach((dicLabel) => {
+    const dicPath = path.join(srcPath, dicLabel);
+    createDic(distPath, dicPath);
+  });
 };
 
-init(srcPath);
+main();
